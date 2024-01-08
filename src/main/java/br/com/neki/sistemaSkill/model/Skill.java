aae3d1d521a2503ad57d63c90562f58fa6bd8996
douglasmariano <douglas.arm@live.com>
2024-01-08 17:17:49 -0300

    criação do sql

--- src/main/java/br/com/neki/sistemaSkill/model/Skill.java
+++ src/main/java/br/com/neki/sistemaSkill/model/Skill.java
@@ -14,7 +14,7 @@ public class Skill {
     @Column(name = "idSkill")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nome;
 
     private String descricao;
---
+++ src/main/resources/sql/SistemaSkill.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE sistemaSkill;
+
+CREATE SEQUENCE seq_usuario START 1;
+
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY DEFAULT nextval('seq_usuario'),
+    nome VARCHAR(255) NOT NULL,
+    login VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE SEQUENCE seq_skill START 1;
+
+CREATE TABLE skill (
+    idSkill INT PRIMARY KEY DEFAULT nextval('seq_skill'),
+    nome VARCHAR(255) UNIQUE NOT NULL,
+    descricao TEXT,
+    imagem VARCHAR(255) NOT NULL
+);
+
+CREATE SEQUENCE seq_usuario_skill START 1;
+
+CREATE TABLE usuario_skill (
+    idUsuarioSkill INT PRIMARY KEY DEFAULT nextval('seq_usuario_skill'),
+    level INT NOT NULL,
+    idUsuario INT REFERENCES usuario(idUsuario),
+    idSkill INT REFERENCES skill(idSkill)
+);
